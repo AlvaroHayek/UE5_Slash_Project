782703e8167fa2902d49705a43204204b31efa1b
HayekAlv <125888859+AlvaroHayek@users.noreply.github.com>
2025-08-16 02:41:22 +0200

    New bird pawn class

--- Slash_Project/Intermediate/Build/Win64/UnrealEditor/Inc/Slash_Project/UHT/Slash_Project.init.gen.cpp
+++ Slash_Project/Intermediate/Build/Win64/UnrealEditor/Inc/Slash_Project/UHT/Slash_Project.init.gen.cpp
@@ -17,13 +17,13 @@ void EmptyLinkFunctionForGeneratedCodeSlash_Project_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x88D10D98,
-				0x908EB252,
+				0xF9A3562D,
+				0xEAC0950C,
 				METADATA_PARAMS(0, nullptr)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_Slash_Project.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_Slash_Project.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Slash_Project(Z_Construct_UPackage__Script_Slash_Project, TEXT("/Script/Slash_Project"), Z_Registration_Info_UPackage__Script_Slash_Project, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x88D10D98, 0x908EB252));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Slash_Project(Z_Construct_UPackage__Script_Slash_Project, TEXT("/Script/Slash_Project"), Z_Registration_Info_UPackage__Script_Slash_Project, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xF9A3562D, 0xEAC0950C));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
