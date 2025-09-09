b544c468cdc8c8c4e103172bf0457256c77dc122
HayekAlv <125888859+AlvaroHayek@users.noreply.github.com>
2025-09-09 03:22:12 +0200

    section 122 part 1

--- Slash_Project/Intermediate/Build/BuildRulesProjects/Slash_ProjectModuleRules/obj/Development/Slash_ProjectModuleRules.AssemblyInfo.cs
+++ Slash_Project/Intermediate/Build/BuildRulesProjects/Slash_ProjectModuleRules/obj/Development/Slash_ProjectModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Slash_ProjectModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb1419a5fe004c5a00221f0307c04f37788d7848")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05f95b7682f7ea3d83222ceb92250f5f1e96332f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Slash_ProjectModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Slash_ProjectModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
