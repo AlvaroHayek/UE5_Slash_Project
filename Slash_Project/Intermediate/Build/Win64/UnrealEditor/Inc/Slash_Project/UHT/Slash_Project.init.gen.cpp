406f4747fa0f24589189cc1300b9ee7f5ab50715
HayekAlv <125888859+AlvaroHayek@users.noreply.github.com>
2025-09-16 15:52:26 +0200

    section 154 finished

--- Slash_Project/Intermediate/Build/Win64/UnrealEditor/Inc/Slash_Project/UHT/Slash_Project.init.gen.cpp
+++ Slash_Project/Intermediate/Build/Win64/UnrealEditor/Inc/Slash_Project/UHT/Slash_Project.init.gen.cpp
@@ -17,13 +17,13 @@ void EmptyLinkFunctionForGeneratedCodeSlash_Project_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x2EFACC44,
-				0xD7A9BE6A,
+				0x82F000AC,
+				0x1A3B30FC,
 				METADATA_PARAMS(0, nullptr)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_Slash_Project.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_Slash_Project.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Slash_Project(Z_Construct_UPackage__Script_Slash_Project, TEXT("/Script/Slash_Project"), Z_Registration_Info_UPackage__Script_Slash_Project, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x2EFACC44, 0xD7A9BE6A));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Slash_Project(Z_Construct_UPackage__Script_Slash_Project, TEXT("/Script/Slash_Project"), Z_Registration_Info_UPackage__Script_Slash_Project, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x82F000AC, 0x1A3B30FC));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ Slash_Project/Intermediate/Build/Win64/UnrealEditor/Inc/Slash_Project/UHT/Treasure.generated.h
@@ -0,0 +1,56 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "Items/Treasure.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef SLASH_PROJECT_Treasure_generated_h
+#error "Treasure.generated.h already included, missing '#pragma once' in Treasure.h"
+#endif
+#define SLASH_PROJECT_Treasure_generated_h
+
+#define FID_Users_Usuario_Desktop_UnrealEngineUnity_UE5_Slash_Project_Slash_Project_Source_Slash_Project_Public_Items_Treasure_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesATreasure(); \
+	friend struct Z_Construct_UClass_ATreasure_Statics; \
+public: \
+	DECLARE_CLASS(ATreasure, AItem, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/Slash_Project"), NO_API) \
+	DECLARE_SERIALIZER(ATreasure)
+
+
+#define FID_Users_Usuario_Desktop_UnrealEngineUnity_UE5_Slash_Project_Slash_Project_Source_Slash_Project_Public_Items_Treasure_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ATreasure(); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	ATreasure(ATreasure&&); \
+	ATreasure(const ATreasure&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ATreasure); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ATreasure); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ATreasure) \
+	NO_API virtual ~ATreasure();
+
+
+#define FID_Users_Usuario_Desktop_UnrealEngineUnity_UE5_Slash_Project_Slash_Project_Source_Slash_Project_Public_Items_Treasure_h_12_PROLOG
+#define FID_Users_Usuario_Desktop_UnrealEngineUnity_UE5_Slash_Project_Slash_Project_Source_Slash_Project_Public_Items_Treasure_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Users_Usuario_Desktop_UnrealEngineUnity_UE5_Slash_Project_Slash_Project_Source_Slash_Project_Public_Items_Treasure_h_15_INCLASS_NO_PURE_DECLS \
+	FID_Users_Usuario_Desktop_UnrealEngineUnity_UE5_Slash_Project_Slash_Project_Source_Slash_Project_Public_Items_Treasure_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> SLASH_PROJECT_API UClass* StaticClass<class ATreasure>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Users_Usuario_Desktop_UnrealEngineUnity_UE5_Slash_Project_Slash_Project_Source_Slash_Project_Public_Items_Treasure_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
