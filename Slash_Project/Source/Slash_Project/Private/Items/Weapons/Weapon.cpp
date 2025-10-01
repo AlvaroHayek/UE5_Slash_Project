1ca1a2f018e73dda9464afa1302562801bf6d9f7
HayekAlv <alvarohayekalfonso@gmail.com>
2025-10-02 00:49:40 +0200

    section 198 finished

--- Slash_Project/Source/Slash_Project/Private/Items/Weapons/Weapon.cpp
+++ Slash_Project/Source/Slash_Project/Private/Items/Weapons/Weapon.cpp
@@ -89,7 +89,7 @@ void AWeapon::AttachMeshToSocket(USceneComponent* InParent, const FName& InSocke
 
 void AWeapon::OnBoxOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	if (ActorIsSameType(OtherActor)) return;
+	//if (ActorIsSameType(OtherActor)) return;
 
 	FHitResult BoxHit;
 	BoxTrace(BoxHit);
