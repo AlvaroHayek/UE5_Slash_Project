9b5ad7df6176ce99cc07aa48268dd497badfe346
HayekAlv <alvarohayekalfonso@gmail.com>
2025-10-09 02:41:17 +0200

    small stuff

---
+++ Slash_Project/Intermediate/Source/Slash_Project.Build.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+
+public class Slash_Project : ModuleRules
+{
+	public Slash_Project(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.Add("Core");
+		PrivateDependencyModuleNames.Add("Core");
+	}
+}
