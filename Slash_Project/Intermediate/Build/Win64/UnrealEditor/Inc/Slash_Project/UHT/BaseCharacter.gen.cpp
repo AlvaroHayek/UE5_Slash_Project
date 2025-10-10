4713c7f5c3448db770b99f5162a4bcd4fc163619
HayekAlv <alvarohayekalfonso@gmail.com>
2025-10-10 19:10:21 +0200

    section 231 finished

--- Slash_Project/Intermediate/Build/Win64/UnrealEditor/Inc/Slash_Project/UHT/BaseCharacter.gen.cpp
+++ Slash_Project/Intermediate/Build/Win64/UnrealEditor/Inc/Slash_Project/UHT/BaseCharacter.gen.cpp
@@ -55,6 +55,48 @@ DEFINE_FUNCTION(ABaseCharacter::execAttackEnd)
 }
 // End Class ABaseCharacter Function AttackEnd
 
+// Begin Class ABaseCharacter Function Die
+static const FName NAME_ABaseCharacter_Die = FName(TEXT("Die"));
+void ABaseCharacter::Die()
+{
+	UFunction* Func = FindFunctionChecked(NAME_ABaseCharacter_Die);
+	if (!Func->GetOwnerClass()->HasAnyClassFlags(CLASS_Native))
+	{
+	ProcessEvent(Func,NULL);
+	}
+	else
+	{
+		Die_Implementation();
+	}
+}
+struct Z_Construct_UFunction_ABaseCharacter_Die_Statics
+{
+#if WITH_METADATA
+	static constexpr UECodeGen_Private::FMetaDataPairParam Function_MetaDataParams[] = {
+		{ "ModuleRelativePath", "Public/Characters/BaseCharacter.h" },
+	};
+#endif // WITH_METADATA
+	static const UECodeGen_Private::FFunctionParams FuncParams;
+};
+const UECodeGen_Private::FFunctionParams Z_Construct_UFunction_ABaseCharacter_Die_Statics::FuncParams = { (UObject*(*)())Z_Construct_UClass_ABaseCharacter, nullptr, "Die", nullptr, nullptr, nullptr, 0, 0, RF_Public|RF_Transient|RF_MarkAsNative, (EFunctionFlags)0x08080C00, 0, 0, METADATA_PARAMS(UE_ARRAY_COUNT(Z_Construct_UFunction_ABaseCharacter_Die_Statics::Function_MetaDataParams), Z_Construct_UFunction_ABaseCharacter_Die_Statics::Function_MetaDataParams) };
+UFunction* Z_Construct_UFunction_ABaseCharacter_Die()
+{
+	static UFunction* ReturnFunction = nullptr;
+	if (!ReturnFunction)
+	{
+		UECodeGen_Private::ConstructUFunction(&ReturnFunction, Z_Construct_UFunction_ABaseCharacter_Die_Statics::FuncParams);
+	}
+	return ReturnFunction;
+}
+DEFINE_FUNCTION(ABaseCharacter::execDie)
+{
+	P_FINISH;
+	P_NATIVE_BEGIN;
+	P_THIS->Die_Implementation();
+	P_NATIVE_END;
+}
+// End Class ABaseCharacter Function Die
+
 // Begin Class ABaseCharacter Function DodgeEnd
 struct Z_Construct_UFunction_ABaseCharacter_DodgeEnd_Statics
 {
@@ -214,6 +256,7 @@ void ABaseCharacter::StaticRegisterNativesABaseCharacter()
 	UClass* Class = ABaseCharacter::StaticClass();
 	static const FNameNativePtrPair Funcs[] = {
 		{ "AttackEnd", &ABaseCharacter::execAttackEnd },
+		{ "Die", &ABaseCharacter::execDie },
 		{ "DodgeEnd", &ABaseCharacter::execDodgeEnd },
 		{ "GetRotationWarpTarget", &ABaseCharacter::execGetRotationWarpTarget },
 		{ "GetTranslationWarpTarget", &ABaseCharacter::execGetTranslationWarpTarget },
@@ -308,6 +351,7 @@ struct Z_Construct_UClass_ABaseCharacter_Statics
 	static UObject* (*const DependentSingletons[])();
 	static constexpr FClassFunctionLinkInfo FuncInfo[] = {
 		{ &Z_Construct_UFunction_ABaseCharacter_AttackEnd, "AttackEnd" }, // 2693290450
+		{ &Z_Construct_UFunction_ABaseCharacter_Die, "Die" }, // 3393099770
 		{ &Z_Construct_UFunction_ABaseCharacter_DodgeEnd, "DodgeEnd" }, // 3423708730
 		{ &Z_Construct_UFunction_ABaseCharacter_GetRotationWarpTarget, "GetRotationWarpTarget" }, // 719767295
 		{ &Z_Construct_UFunction_ABaseCharacter_GetTranslationWarpTarget, "GetTranslationWarpTarget" }, // 2649329017
@@ -396,10 +440,10 @@ ABaseCharacter::~ABaseCharacter() {}
 struct Z_CompiledInDeferFile_FID_Users_Usuario_Desktop_UnrealEngineUnity_UE5_Slash_Project_Slash_Project_Source_Slash_Project_Public_Characters_BaseCharacter_h_Statics
 {
 	static constexpr FClassRegisterCompiledInInfo ClassInfo[] = {
-		{ Z_Construct_UClass_ABaseCharacter, ABaseCharacter::StaticClass, TEXT("ABaseCharacter"), &Z_Registration_Info_UClass_ABaseCharacter, CONSTRUCT_RELOAD_VERSION_INFO(FClassReloadVersionInfo, sizeof(ABaseCharacter), 4198038633U) },
+		{ Z_Construct_UClass_ABaseCharacter, ABaseCharacter::StaticClass, TEXT("ABaseCharacter"), &Z_Registration_Info_UClass_ABaseCharacter, CONSTRUCT_RELOAD_VERSION_INFO(FClassReloadVersionInfo, sizeof(ABaseCharacter), 638651064U) },
 	};
 };
-static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_Users_Usuario_Desktop_UnrealEngineUnity_UE5_Slash_Project_Slash_Project_Source_Slash_Project_Public_Characters_BaseCharacter_h_3760153491(TEXT("/Script/Slash_Project"),
+static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_Users_Usuario_Desktop_UnrealEngineUnity_UE5_Slash_Project_Slash_Project_Source_Slash_Project_Public_Characters_BaseCharacter_h_1568104556(TEXT("/Script/Slash_Project"),
 	Z_CompiledInDeferFile_FID_Users_Usuario_Desktop_UnrealEngineUnity_UE5_Slash_Project_Slash_Project_Source_Slash_Project_Public_Characters_BaseCharacter_h_Statics::ClassInfo, UE_ARRAY_COUNT(Z_CompiledInDeferFile_FID_Users_Usuario_Desktop_UnrealEngineUnity_UE5_Slash_Project_Slash_Project_Source_Slash_Project_Public_Characters_BaseCharacter_h_Statics::ClassInfo),
 	nullptr, 0,
 	nullptr, 0);
--- Slash_Project/Intermediate/Build/Win64/UnrealEditor/Inc/Slash_Project/UHT/Enemy.gen.cpp
+++ Slash_Project/Intermediate/Build/Win64/UnrealEditor/Inc/Slash_Project/UHT/Enemy.gen.cpp
@@ -302,10 +302,10 @@ AEnemy::~AEnemy() {}
 struct Z_CompiledInDeferFile_FID_Users_Usuario_Desktop_UnrealEngineUnity_UE5_Slash_Project_Slash_Project_Source_Slash_Project_Public_Enemy_Enemy_h_Statics
 {
 	static constexpr FClassRegisterCompiledInInfo ClassInfo[] = {
-		{ Z_Construct_UClass_AEnemy, AEnemy::StaticClass, TEXT("AEnemy"), &Z_Registration_Info_UClass_AEnemy, CONSTRUCT_RELOAD_VERSION_INFO(FClassReloadVersionInfo, sizeof(AEnemy), 349596124U) },
+		{ Z_Construct_UClass_AEnemy, AEnemy::StaticClass, TEXT("AEnemy"), &Z_Registration_Info_UClass_AEnemy, CONSTRUCT_RELOAD_VERSION_INFO(FClassReloadVersionInfo, sizeof(AEnemy), 2051412790U) },
 	};
 };
-static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_Users_Usuario_Desktop_UnrealEngineUnity_UE5_Slash_Project_Slash_Project_Source_Slash_Project_Public_Enemy_Enemy_h_1788348415(TEXT("/Script/Slash_Project"),
+static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_Users_Usuario_Desktop_UnrealEngineUnity_UE5_Slash_Project_Slash_Project_Source_Slash_Project_Public_Enemy_Enemy_h_1767020408(TEXT("/Script/Slash_Project"),
 	Z_CompiledInDeferFile_FID_Users_Usuario_Desktop_UnrealEngineUnity_UE5_Slash_Project_Slash_Project_Source_Slash_Project_Public_Enemy_Enemy_h_Statics::ClassInfo, UE_ARRAY_COUNT(Z_CompiledInDeferFile_FID_Users_Usuario_Desktop_UnrealEngineUnity_UE5_Slash_Project_Slash_Project_Source_Slash_Project_Public_Enemy_Enemy_h_Statics::ClassInfo),
 	nullptr, 0,
 	nullptr, 0);
