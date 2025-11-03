20a2b8025654b8c812b7d9dc17b2a942c68ab5d9
HayekAlv <alvarohayekalfonso@gmail.com>
2025-11-04 00:26:29 +0100

    more

--- Slash_Project/Source/Slash_Project/Public/SlotMachine/SlotMachine.h
+++ Slash_Project/Source/Slash_Project/Public/SlotMachine/SlotMachine.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "Interfaces/HitInterface.h"
+#include "SlotMachine/SlotTypes.h"
 #include "SlotMachine.generated.h"
 
 class UCapsuleComponent;
---
+++ Slash_Project/Source/Slash_Project/Public/SlotMachine/SlotTypes.h
@@ -0,0 +1,9 @@
+#pragma once
+
+UENUM(BlueprintType)
+enum class ESlotResult : uint8
+{
+	ESR_SlotResult1 UMETA(DisplayName = "SlotResult1"),
+	ESR_SlotResult2 UMETA(DisplayName = "SlotResult2"),
+	ESR_SlotResult3 UMETA(DisplayName = "SlotResult3")
+};
